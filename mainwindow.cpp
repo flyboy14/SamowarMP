441fea3e9344aa4b8ef538daefa9163af37b30f5
flyboy14 <say.bzzzzz@gmail.com>
2015-02-19 23:05:14 +0300

    still cmdl_args fixes 4

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -375,6 +375,9 @@ void MainWindow::on_actionClear_playlist_triggered()
 void MainWindow::on_dialVolume_valueChanged(int value)
 {
     plr -> setVolume(value);
+    char *buffer = new char[3];
+    sprintf(buffer,"%d",value);
+    ui->labelVolumeDef -> setText(buffer);
 }
 
 void MainWindow::on_dialVolume_sliderPressed()
@@ -524,7 +527,7 @@ void MainWindow::add_files_from_behind()
                         if(cmdline_args.count() == 0) return;
                     }
                 }
-                if(cmdline_args.at(v) == "-v" || cmdline_args.at(v) == "--volume") {
+                else if(cmdline_args.at(v) == "-v" || cmdline_args.at(v) == "--volume") {
                     ui->dialVolume->setValue(cmdline_args.at(v+1).toInt());
                     cmdline_args.removeAt(v);
                     cmdline_args.removeAt(v);
